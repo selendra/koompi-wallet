abc6288902d6ffc0dd81c9756878852406008fb9
Piset Heang <piset2cambo@gmail.com>
2020-02-27 16:30:42 +0700

    init

---
+++ components/Navbar.vue
@@ -0,0 +1,124 @@
+<template>
+  <nav class="Desktop">
+    <v-app-bar
+      dense
+      fixed
+      app
+    >
+      <v-btn text @click="handleDrawer()" @click.stop="mini = !mini">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+      <v-toolbar-title>
+        <img src="../assets/koompi_title.png" alt="logo_ke" style="width: 140px">
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text @click="handleSignout()">
+        <span>SignOut</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      permanent
+      app
+    >
+      <v-list>
+        <v-list-item-group>
+          <div style="padding-top: 3rem"></div>
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-wallet</v-icon>
+              <!-- <img src="" alt=" wallet" style="width: 25px"> -->
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium">Wallet</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/transaction">
+            <v-list-item-icon>
+              <!-- <img src="" alt=" wallet" style="width: 25px"> -->
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium">Transaction</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/send">
+            <v-list-item-icon>
+              <img src="" alt=" wallet" style="width: 25px">
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium">Send</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="receive">
+            <v-list-item-icon>
+              <img src="" alt=" wallet" style="width: 25px">
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium">Receive</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="setting">
+            <v-list-item-icon>
+              <img src="" alt=" wallet" style="width: 25px">
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium">Setting</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      mini: true,
+    }
+  },
+  methods: {
+    handleDrawer() {
+      this.drawer = !this.drawer;
+    },
+    handleSignout() {
+      this.$store.dispatch('users/handleLogout');
+    },
+  }
+}
+</script>
+
+<style scoped>
+  @media only screen and (max-width: 500px) {
+    .Desktop {
+      display: none!important;
+    }
+  }
+  /* //Tablet */
+  @media only screen and (min-width: 501px) and (max-width: 767px) {
+    .Desktop {
+      display: none!important;
+    }
+  }
+  /* //Normal */
+  @media only screen and (min-width: 768px) and (max-width: 1199px){
+    
+  }
+  /* Large monitor */
+  @media only screen and (min-width: 1200px) and (max-width: 1919px) {
+        
+  }
+  /* //Landscape */
+  @media only screen and (max-height: 500px) {
+      
+  }
+  /* Widescreen */
+  @media only screen and (min-width: 1920px) {
+      
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,104 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  env: {
+    KEUrl: 'https://testnet-api.zeetomic.com/ke/v1',
+    apiUrl: 'https://testnet-api.zeetomic.com/pub/v1'
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/toast',
+    '@nuxtjs/pwa'
+  ],
+  toast: {
+    position: 'top-center',
+    theme: "bubble", 
+    duration: '3000',
+    register: [ // Register custom toasts
+      {
+        name: 'my-error',
+        message: 'Oops...Something went wrong',
+        options: {
+          type: 'error'
+        }
+      }
+    ]
+  },
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  icon: {
+    iconSrc: './assets/koompi_logo512.png',
+  },
+  manifest: {
+    short_name: 'Koompi Wallet',
+    name: 'Koompi Wallet',
+    start_url: '/',
+    theme_color: '#222834',
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
